c487ac5ebbd6fd74eb6929ccfae02a5f1b63705c
michalkoziara <mkoziara12@gmail.com>
2020-04-11 14:01:09 +0200

    Add material design module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ServiceWorkerModule} from '@angular/service-worker';
 
 import {AppComponent} from './app.component';
+import {environment} from '../environments/environment';
+import {AppRoutingModule} from './app-routing.module';
 import {CoreModule} from './core/core.module';
 import {SharedModule} from './shared/shared.module';
-import {AppRoutingModule} from './app-routing.module';
-import {ServiceWorkerModule} from '@angular/service-worker';
-import {environment} from '../environments/environment';
+import {MaterialModule} from './angular-material.module';
 
 @NgModule({
   declarations: [
@@ -14,10 +16,11 @@ import {environment} from '../environments/environment';
   ],
   imports: [
     BrowserModule,
+    MaterialModule,
     AppRoutingModule,     // Main routes for the application
     CoreModule,           // Singleton objects (services, components that are loaded once)
     SharedModule,         // Shared (multi-instance) objects
-    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production})
+    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production}), BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
