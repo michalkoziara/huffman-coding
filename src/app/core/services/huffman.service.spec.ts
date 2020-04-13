5bb7de06173f9cd5b260b4bfed6b2ad8c9cd37f8
michalkoziara <mkoziara12@gmail.com>
2020-04-13 20:37:48 +0200

    Add Huffman tree creating method

--- src/app/core/services/huffman.service.spec.ts
+++ src/app/core/services/huffman.service.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { HuffmanService } from './huffman.service';
+import {HuffmanService} from './huffman.service';
 
 describe('HuffmanService', () => {
   let service: HuffmanService;
@@ -12,5 +12,27 @@ describe('HuffmanService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('#createFrequencyTable should return frequency map', () => {
+    expect(service.createFrequencyTable('test')).toEqual(new Map([['t', 2], ['e', 1], ['s', 1]]));
+  });
+
+  it('#createHuffmanTree should return Huffman tree', () => {
+    expect(service.createHuffmanTree(
+      new Map([['t', 2], ['e', 1], ['s', 1]]))
+    ).toEqual(
+      {
+        value: 4, text: 'est', left: {value: 1, text: 'e', code: '0'},
+        right: {
+          value: 3,
+          text: 'st',
+          left: {value: 1, text: 's', code: '10'},
+          right: {value: 2, text: 't', code: '11'},
+          code: '1'
+        },
+        code: ''
+      }
+    );
   });
 });
--- src/app/core/services/huffman.service.ts
+++ src/app/core/services/huffman.service.ts
@@ -1,8 +1,127 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Tree} from './tree';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HuffmanService {
+
+  createFrequencyTable(text: string): Map<string, number> {
+    const frequencyTable: Map<string, number> = new Map<string, number>();
+    for (let i = 0; i < text.length; i++) {
+      const charFrequency = frequencyTable.get(text.charAt(i));
+      if (charFrequency != undefined) {
+        frequencyTable.set(text.charAt(i), charFrequency + 1);
+      } else {
+        frequencyTable.set(text.charAt(i), 1)
+      }
+    }
+
+    return frequencyTable;
+  }
+
+  createHuffmanTree(frequencyTable: Map<string, number>): Tree | null {
+    const frequencyTableCopy: Map<string, number> = new Map<string, number>(frequencyTable);
+
+    let huffmanTree: Tree | null = null;
+    if (frequencyTableCopy.size == 0) {
+      return huffmanTree;
+    }
+
+    if (frequencyTableCopy.size == 1) {
+      huffmanTree = {
+        value: 1,
+        right: {value: 1, text: frequencyTableCopy.keys().next().value, code: '1'}
+      };
+      return huffmanTree;
+    }
+
+    huffmanTree = this.createHuffmanTreeWithMultipleNodes(frequencyTableCopy);
+    if (huffmanTree) {
+      this.addCodesToHuffmanTree(huffmanTree);
+    }
+
+    return huffmanTree;
+  }
+
+  private createHuffmanTreeWithMultipleNodes(frequencyTable: Map<string, number>): Tree {
+    const huffmanTreeByText: Map<string, Tree> = new Map<string, Tree>();
+    while (frequencyTable.size > 1) {
+      let minLeftValue: number | null = null;
+      let minLeft: string | null = null;
+      let minRightValue: number | null = null;
+      let minRight: string | null = null;
+
+      for (const [key, value] of frequencyTable) {
+        if (minLeft == null) {
+          minLeft = key;
+          minLeftValue = value;
+        } else if (minRight == null) {
+          minRight = key;
+          minRightValue = value;
+        } else if (minLeftValue != null && minLeftValue >= value) {
+          minRightValue = minLeftValue;
+          minRight = minLeft;
+          minLeftValue = value;
+          minLeft = key;
+        }
+      }
+
+      if (minLeft != null && minRight != null && minLeftValue != null && minRightValue != null) {
+        frequencyTable.delete(minLeft);
+        frequencyTable.delete(minRight);
+        frequencyTable.set(minLeft + minRight, minLeftValue + minRightValue);
+
+        const currentHuffmanTree: Tree = {
+          value: minLeftValue + minRightValue,
+          text: minLeft + minRight
+        };
+
+        if (huffmanTreeByText.has(minLeft)) {
+          currentHuffmanTree.left = huffmanTreeByText.get(minLeft);
+          huffmanTreeByText.delete(minLeft);
+        } else {
+          currentHuffmanTree.left = {value: minLeftValue, text: minLeft};
+        }
+
+        if (huffmanTreeByText.has(minRight)) {
+          currentHuffmanTree.right = huffmanTreeByText.get(minRight);
+          huffmanTreeByText.delete(minRight);
+        } else {
+          currentHuffmanTree.right = {value: minRightValue, text: minRight};
+        }
+
+        huffmanTreeByText.set(minLeft + minRight, currentHuffmanTree);
+      }
+    }
+
+    return huffmanTreeByText.values().next().value;
+  }
+
+  private addCodesToHuffmanTree(huffmanTree: Tree): Tree {
+    return this.recursivelyAddCodesToHuffmanTree(huffmanTree);
+  }
+
+  private recursivelyAddCodesToHuffmanTree(huffmanTree: Tree, side?: string, code?: string): Tree {
+    if (side) {
+      if (side == 'left') {
+        huffmanTree.code = code + '0';
+      } else {
+        huffmanTree.code = code + '1';
+      }
+    } else {
+      huffmanTree.code = '';
+    }
+
+    if (huffmanTree.left) {
+      this.recursivelyAddCodesToHuffmanTree(huffmanTree.left, 'left', huffmanTree.code);
+    }
+
+    if (huffmanTree.right) {
+      this.recursivelyAddCodesToHuffmanTree(huffmanTree.right, 'right', huffmanTree.code);
+    }
+
+    return huffmanTree;
+  }
 
 }
