36b542a5006012d5b9adf07da9e25496d292825b
michalkoziara <mkoziara12@gmail.com>
2020-04-13 21:16:44 +0200

    Add navigation routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {FgkComponent} from './core/fgk/fgk.component';
+import {HuffmanComponent} from './core/huffman/huffman.component';
+import {VitterComponent} from './core/vitter/vitter.component';
 
 const appRoutes: Routes = [
+  {path: 'fgk', component: FgkComponent},
+  {path: 'huffman', component: HuffmanComponent},
+  {path: 'vitter', component: VitterComponent},
   {path: '**', pathMatch: 'full', redirectTo: '/'}
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {MatSidenav} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  currentSite = 'Kodowanie Huffmana - wprowadzenie';
+
+  @ViewChild(MatSidenav) matSidenav: MatSidenav | undefined;
+
+  menuListItemSelected(routerLink: string): void {
+    if (this.matSidenav) {
+      this.matSidenav.close();
+    }
+
+    if (routerLink == '/') {
+      this.currentSite = 'Kodowanie Huffmana - wprowadzenie';
+    }
+
+    if (routerLink == '/huffman') {
+      this.currentSite = 'Kodowanie Huffmana';
+    }
+
+    if (routerLink == '/fgk') {
+      this.currentSite = 'Algorytm Fallera-Gallagera-Knutha';
+    }
+
+    if (routerLink == '/vitter') {
+      this.currentSite = 'Algorytm Vittera';
+    }
+  }
 }
