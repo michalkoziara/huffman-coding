6cbfdd483b24c8a42e0bb309d09f851d6293e58c
michalkoziara <mkoziara12@gmail.com>
2020-05-15 23:17:07 +0200

    Add description to logic related to Huffman coding

--- src/app/core/services/huffman.service.ts
+++ src/app/core/services/huffman.service.ts
@@ -6,6 +6,11 @@ import {Tree} from './tree';
 })
 export class HuffmanService {
 
+  /**
+   * Creates a frequency map of each letter in the given text.
+   * @param {string} text text that should have frequency map calculated for
+   * @returns frequency table
+   */
   createFrequencyTable(text: string): Map<string, number> {
     const frequencyTable: Map<string, number> = new Map<string, number>();
     for (let i = 0; i < text.length; i++) {
@@ -20,6 +25,11 @@ export class HuffmanService {
     return frequencyTable;
   }
 
+  /**
+   * Creates Huffman tree using basic Huffman coding.
+   * @param {Map} frequencyTable map that contains frequency of occurs of letters
+   * @returns Huffman tree
+   */
   createHuffmanTree(frequencyTable: Map<string, number>): Tree | null {
     const frequencyTableCopy: Map<string, number> = new Map<string, number>(frequencyTable);
 
@@ -29,6 +39,7 @@ export class HuffmanService {
     }
 
     if (frequencyTableCopy.size == 1) {
+      // Tree contains only one node so it have to be added on the one side of tree
       huffmanTree = {
         value: 1,
         right: {value: 1, text: frequencyTableCopy.keys().next().value, code: '1'}
@@ -44,64 +55,96 @@ export class HuffmanService {
     return huffmanTree;
   }
 
+  /**
+   * Creates every node of Huffman tree that contains more than one node using basic Huffman coding.
+   * @param {Map} frequencyTable map that contains frequency of occurs of letters
+   * @returns Huffman tree
+   */
   private createHuffmanTreeWithMultipleNodes(frequencyTable: Map<string, number>): Tree {
     const huffmanTreeByText: Map<string, Tree> = new Map<string, Tree>();
-    while (frequencyTable.size > 1) {
-      let minLeftValue: number | null = null;
-      let minLeft: string | null = null;
-      let minRightValue: number | null = null;
-      let minRight: string | null = null;
 
-      for (const [key, value] of frequencyTable) {
-        if (minLeft == null) {
-          minLeft = key;
-          minLeftValue = value;
-        } else if (minRight == null) {
-          minRight = key;
-          minRightValue = value;
-        } else if (minLeftValue != null && minLeftValue >= value) {
-          minRightValue = minLeftValue;
-          minRight = minLeft;
-          minLeftValue = value;
-          minLeft = key;
+    // Each letter should have corresponding node in Huffman tree
+    while (frequencyTable.size > 1) {
+      let minValueOnLeft: number | null = null;
+      let letterWithMinValueOnLeft: string | null = null;
+      let minValueOnRight: number | null = null;
+      let letterWithMinValueOnRight: string | null = null;
+
+      // Gets two letters with the lowest values in frequency table to put into right and left node
+      for (const [letter, value] of frequencyTable) {
+
+        // This is first iteration so left node should be set
+        if (letterWithMinValueOnLeft == null) {
+          letterWithMinValueOnLeft = letter;
+          minValueOnLeft = value;
+
+          // This is second iteration so right node should be set
+        } else if (letterWithMinValueOnRight == null) {
+          letterWithMinValueOnRight = letter;
+          minValueOnRight = value;
+
+          // For other iterations if current letter have larger value then it should be replaced
+        } else if (minValueOnLeft != null && minValueOnLeft >= value) {
+          minValueOnRight = minValueOnLeft;
+          letterWithMinValueOnRight = letterWithMinValueOnLeft;
+          minValueOnLeft = value;
+          letterWithMinValueOnLeft = letter;
         }
       }
 
-      if (minLeft != null && minRight != null && minLeftValue != null && minRightValue != null) {
-        frequencyTable.delete(minLeft);
-        frequencyTable.delete(minRight);
-        frequencyTable.set(minLeft + minRight, minLeftValue + minRightValue);
+      // Two letters with lowest value should have node created and be merged in the frequency table
+      if (letterWithMinValueOnLeft != null && letterWithMinValueOnRight != null
+        && minValueOnLeft != null && minValueOnRight != null) {
+
+        frequencyTable.delete(letterWithMinValueOnLeft);
+        frequencyTable.delete(letterWithMinValueOnRight);
+        frequencyTable.set(letterWithMinValueOnLeft + letterWithMinValueOnRight,
+          minValueOnLeft + minValueOnRight);
 
         const currentHuffmanTree: Tree = {
-          value: minLeftValue + minRightValue,
-          text: minLeft + minRight
+          value: minValueOnLeft + minValueOnRight,
+          text: letterWithMinValueOnLeft + letterWithMinValueOnRight
         };
 
-        if (huffmanTreeByText.has(minLeft)) {
-          currentHuffmanTree.left = huffmanTreeByText.get(minLeft);
-          huffmanTreeByText.delete(minLeft);
+        // The letter can consist of several letters with a related subtree, then adds subtree into current tree
+        if (huffmanTreeByText.has(letterWithMinValueOnLeft)) {
+          currentHuffmanTree.left = huffmanTreeByText.get(letterWithMinValueOnLeft);
+          huffmanTreeByText.delete(letterWithMinValueOnLeft);
         } else {
-          currentHuffmanTree.left = {value: minLeftValue, text: minLeft};
+          currentHuffmanTree.left = {value: minValueOnLeft, text: letterWithMinValueOnLeft};
         }
 
-        if (huffmanTreeByText.has(minRight)) {
-          currentHuffmanTree.right = huffmanTreeByText.get(minRight);
-          huffmanTreeByText.delete(minRight);
+        if (huffmanTreeByText.has(letterWithMinValueOnRight)) {
+          currentHuffmanTree.right = huffmanTreeByText.get(letterWithMinValueOnRight);
+          huffmanTreeByText.delete(letterWithMinValueOnRight);
         } else {
-          currentHuffmanTree.right = {value: minRightValue, text: minRight};
+          currentHuffmanTree.right = {value: minValueOnRight, text: letterWithMinValueOnRight};
         }
 
-        huffmanTreeByText.set(minLeft + minRight, currentHuffmanTree);
+        huffmanTreeByText.set(letterWithMinValueOnLeft + letterWithMinValueOnRight, currentHuffmanTree);
       }
     }
 
     return huffmanTreeByText.values().next().value;
   }
 
+  /**
+   * Adds code to each node of Huffman tree.
+   * @param huffmanTree Huffman tree that should have codes added
+   * @returns Huffman tree with codes added
+   */
   private addCodesToHuffmanTree(huffmanTree: Tree): Tree {
     return this.recursivelyAddCodesToHuffmanTree(huffmanTree);
   }
 
+  /**
+   * Adds code to nodes of Huffman tree depending on the side of node and its parent's code.
+   * This method is run recursively for left and right side of tree.
+   * @param huffmanTree Huffman tree that should have codes added
+   * @param side the side of the tree where the node is located. Binary tree contains left and right leaves.
+   * @param code code of node's parent
+   * @returns Huffman tree with codes added
+   */
   private recursivelyAddCodesToHuffmanTree(huffmanTree: Tree, side?: string, code?: string): Tree {
     if (side) {
       if (side == 'left') {
--- src/app/core/services/tree.ts
+++ src/app/core/services/tree.ts
@@ -1,3 +1,12 @@
+/**
+ * Interface representing Huffman tree.
+ * @property {number} value value of this node
+ * @property {string} text letter or several letters that this node represents
+ * @property {string} code Huffman code of this node
+ * @property {Tree} left left leaf
+ * @property {Tree} right right leaf
+ * @property {Tree} parentNode root node
+ */
 export interface Tree {
   value: number;
   text?: string;
